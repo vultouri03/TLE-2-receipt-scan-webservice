9416c2b8b8b1ad0cec5913fbc602a0978940dc35
vultouri03 <youridegier@gmail.com>
2024-04-16 11:25:34 +0200

    fixed a bug that allowed the sending of empty data
    added an if statement to check if the image data has been filled, if it has not it will send back a default array

--- router.js
+++ router.js
@@ -1,11 +1,14 @@
 import express from "express";
 
+//creates a router which loads the routes
 const router = express.Router();
 
+//the get route it sends back a check to show that the app is working
 router.get("/", async(req, res) => {
     res.json("we are classefied")
 })
 
+//post requests creates an empty categories array and loads the data from the request so that it can be used with tesseract.js
 router.post("/", async(req, res)=> {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -19,14 +22,24 @@ router.post("/", async(req, res)=> {
         "sauces": 0
     };
 
+
     const image = req.body.image;
 
-    console.log(atob(image));
 
-    categories.dairy += 10;
+    //checks if the image base 64 has been sent
+    if (req.body.image) {
+        console.log("hello world")
+        console.log(atob(image));
+    } else {
+        //fills the array with dummy data in case the image hasn't been sent yet
+        categories.dairy += 10;
+    }
+
+
 
     res.json(categories)
 
 })
 
+//exports the router
 export default router
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from "express"
 import router from "./router.js";
 
+//creates a basic express app
 const app = express();
 
+//allows the use of json and urlencoded requests
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
 
+//loads the routes from the /classify url
 app.use("/classify", router)
 
+//opens the app on port 8000
 app.listen("8000")
 
